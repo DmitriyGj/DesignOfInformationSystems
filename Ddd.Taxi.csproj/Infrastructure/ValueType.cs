3fec3f0dae522a848c0e2a4106c55d0c9f57bcb3
DmitriyGj <71816721+DmitriyGj@users.noreply.github.com>
2021-05-24 18:48:04 +0500

    New

---
+++ Ddd.Taxi.csproj/Infrastructure/ValueType.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace Ddd.Infrastructure
+{
+	/// <summary>
+	/// Базовый класс для всех Value типов.
+	/// </summary>
+	public class ValueType<T>
+	{
+		public override bool Equals(object obj)
+        {
+			if (obj is null ||!obj.GetType().Equals( typeof(T)))
+				return false;
+			var objT = obj.GetType();
+			var type = typeof(T);
+
+			if (object.ReferenceEquals(obj, this))
+				return true;
+			return type.GetProperties().All(prop => prop.GetValue(this).Equals(prop.GetValue(obj)));
+        }
+			
+
+        public override int GetHashCode()
+        {
+			int basePrimal = unchecked(0);
+			foreach (var prop in typeof(T).GetFields())
+			unchecked
+			{
+				basePrimal += prop.GetValue(this).GetHashCode();
+			};
+			return basePrimal;
+        }
+
+        public override string ToString()
+        {
+			var fields = typeof(T).GetProperties();
+			return $"{typeof(T).Name}({string.Join("; ", fields.Select(s => $"{s.Name}: {s.GetValue(this,null)}"))})";
+		}
+    }
+}
