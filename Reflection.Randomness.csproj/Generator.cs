b996c674b528d2fea1a700c8e41357ad6110c5ea
DmitriyGj <71816721+DmitriyGj@users.noreply.github.com>
2021-05-27 20:08:18 +0500

    New

--- Reflection.Randomness.csproj/Generator.cs
+++ Reflection.Randomness.csproj/Generator.cs
@@ -7,4 +7,36 @@ using System.Reflection;
 
 namespace Reflection.Randomness
 {
+    public class Generator<T> where T:new()
+    {
+        Type type;
+        Dictionary<PropertyInfo, FromDistribution> propertiesWithAttributes;
+        public T Generate(Random rnd)
+        {
+            var resultDistribution = new T();
+            foreach (var pwa in propertiesWithAttributes)
+                pwa.Key.SetValue(resultDistribution, pwa.Value.Distribution.Generate(rnd));
+            return resultDistribution;
+        }
+
+        public Generator()
+        {
+            type = typeof(T);
+            propertiesWithAttributes = type.GetProperties()
+                .Where(prop => prop.GetCustomAttributes()
+                .OfType<FromDistribution>().Count() > 0)
+                .ToDictionary(prop => prop, prop => prop.GetCustomAttributes().OfType<FromDistribution>().First());
+        }
+    }
+
+    public class FromDistribution : Attribute
+    {
+        public IContinuousDistribution Distribution { get; set; }
+        public FromDistribution(Type distributionType, params object[] parametrs)
+        {
+            if (!distributionType.GetConstructors().Any(ctor=>ctor.GetParameters().Count() == parametrs.Length))
+                throw new ArgumentException(distributionType.ToString());
+            Distribution = Activator.CreateInstance(distributionType,parametrs) as IContinuousDistribution;
+        }
+    }
 }
