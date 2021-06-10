9624f3aed0e49a2491b288bc93ee4e558c21911b
DmitriyGj <71816721+DmitriyGj@users.noreply.github.com>
2021-06-10 23:58:55 +0500

    New

--- Reflection.Differentiation.csproj/Algebra.cs
+++ Reflection.Differentiation.csproj/Algebra.cs
@@ -1,9 +1,13 @@
 ﻿using System;
+using System.CodeDom;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Linq.Expressions;
+using  System.Reflection;
 
 namespace Reflection.Differentiation
 {
@@ -11,12 +15,52 @@ namespace Reflection.Differentiation
     {
         public static Expression<Func<double,double>> Differentiate(Expression<Func<double,double>> func)
         {
-            if(func is BinaryExpression)
-            {
-                var node = (BinaryExpression)func;
-                if(node.Left is ParameterExpression)
+            var param = func.Parameters;
+            var result = GetDifferentiate(func.Body);
+            return Expression.Lambda<Func<double,double>>(result,param);
+        }
 
+        public static Expression GetDifferentiate(Expression func)
+        {
+            var nodeType = func.NodeType;
+            if (nodeType is ExpressionType.Constant)
+                return Expression.Constant(0.0);
+            if (nodeType is ExpressionType.Parameter)
+                return Expression.Constant(1.0);
+            if (nodeType is ExpressionType.Add)
+            {
+                var left = GetDifferentiate((func as BinaryExpression).Left);
+                var right = GetDifferentiate((func as BinaryExpression).Right);
+                return Expression.Add(left, right);
             }
+
+            if (nodeType is ExpressionType.Multiply)
+            {
+                var likeBinary = func as BinaryExpression;
+                var left = GetDifferentiate(likeBinary.Left);
+                var right = GetDifferentiate(likeBinary.Right);
+                return Expression.Add(Expression.Multiply(left, likeBinary.Right),
+                    Expression.Multiply(right, likeBinary.Left));
+            }
+
+            if (nodeType is ExpressionType.Call)
+            {
+                var call = (func as MethodCallExpression);
+                var body = call.Arguments.First();
+                if (call.Method.Name == "Cos")
+                {
+                    return Expression.Multiply(Expression.Multiply(Expression.Constant(-1.0),
+                        Expression.Call(typeof(Math).GetMethod("Sin"),body)),
+                        GetDifferentiate(body));
+                }
+
+                if (call.Method.Name == "Sin")
+                {
+                    return Expression.Multiply(Expression.Call(typeof(Math).GetMethod("Cos"),body),
+                        GetDifferentiate(body));
+                }
+            }
+            throw new ArgumentException(func.ToString());
         }
     }   
 }
