73ce1f35e6fc14afbb8a93d73000f0f39ad3358f
DmitriyGj <71816721+DmitriyGj@users.noreply.github.com>
2021-06-25 21:02:35 +0500

    New

---
+++ Memory.API.csproj/APIObject.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Memory.API
+{
+    public class APIObject : IDisposable
+    {
+        private bool disposedValue;
+        private int number;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    Dispose();
+                }
+
+                // TODO: освободить неуправляемые ресурсы (неуправляемые объекты) и переопределить метод завершения
+                // TODO: установить значение NULL для больших полей
+                disposedValue = true;
+            }
+        }
+
+        // // TODO: переопределить метод завершения, только если "Dispose(bool disposing)" содержит код для освобождения неуправляемых ресурсов
+        ~APIObject()
+         {
+            Dispose(true);
+        //     // Не изменяйте этот код. Разместите код очистки в методе "Dispose(bool disposing)".
+        //   
+         }
+
+        public APIObject(int n)
+        {
+            number = n;
+        }
+
+        void IDisposable.Dispose()
+        {
+            // Не изменяйте этот код. Разместите код очистки в методе "Dispose(bool disposing)".
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
